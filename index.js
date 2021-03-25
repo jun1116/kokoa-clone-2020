79c53bb7ac19d89ab1a42f69bd64b5416d69af12
jun1116 <52767291+jun1116@users.noreply.github.com>
2021-03-25 11:36:17 +0900

    save

---
+++ index.js
@@ -0,0 +1,6 @@
+const times = document.getElementById('current-time');
+const start = new Date();
+var hour = start.getHours();
+var min = start.getMinutes();
+var ctime = hour + ':' + min;
+console.log(times)
